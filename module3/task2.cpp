f52e2507247f5433386cbc1d1d342bf524a2ad05
gavroman <gavroman1998@gmail.com>
2019-12-18 00:08:30 +0300

    Release done

--- module3/task2.cpp
+++ module3/task2.cpp
@@ -13,10 +13,8 @@
 
 #include <assert.h>
 #include <bits/stdc++.h>
-#include <iomanip>
 #include <iostream>
 #include <queue>
-#include <sstream>
 #include <vector>
 
 class ListGraph{
@@ -47,22 +45,15 @@ class ListGraph{
     int verticesCount;
 };
 
-template <class T>
-std::ostream& operator<<(std::ostream &out, const std::vector<T>& v) {
-    for (const auto elem : v) {
-        out << std::setw(3) << elem << " ";
-    }
-    return out;
-}
 
 int countShortestPaths(ListGraph& graph, int startVertex, int endVertex) {
     int verticesCount = graph.size();
+    std::vector<int> pathsNumber(verticesCount, 0);
     std::vector<int> distances(verticesCount, INT_MAX);
-    std::vector<int> paths_number(verticesCount, 0);
-    std::vector<int> parents(verticesCount, -1);
+    distances[startVertex] = 0;
+    pathsNumber[startVertex] = 1;
     std::queue<int> q;
     q.push(startVertex);
-    distances[startVertex] = 0;
     while(!q.empty()) {
         int currentVertex = q.front();
         q.pop();
@@ -70,147 +61,37 @@ int countShortestPaths(ListGraph& graph, int startVertex, int endVertex) {
         for (const auto& nextVertex : nextVertices) {
             if (distances[nextVertex] == INT_MAX) {
                 distances[nextVertex] = distances[currentVertex] + 1;
-                paths_number[nextVertex] = paths_number[currentVertex];
-                parents[nextVertex] = currentVertex;
+                pathsNumber[nextVertex] = pathsNumber[currentVertex];
                 q.push(nextVertex);
             } else if (distances[currentVertex] + 1 == distances[nextVertex]) {
-                distances[nextVertex] = distances[currentVertex] + 1;
-                paths_number[nextVertex] += paths_number[currentVertex];
-                parents[nextVertex] = currentVertex;
+                pathsNumber[nextVertex] += pathsNumber[currentVertex];
             }
         }
     }
-    // std::cout << "parents: " << parents << std::endl;
-    // std::cout << "distances: " << distances << std::endl;
 
-    // std::cout << "shortest distance : " << distances[endVertex] << std::endl;
-    // std::cout << "paths count : " << paths_number[endVertex] << std::endl;
-
-    std::cout << paths_number[endVertex] << std::endl;
-
-    // int currentVertex = endVertex;
-    // while (currentVertex != startVertex) {
-    //     std::cout << currentVertex << "<-";
-    //     currentVertex = parents[currentVertex];
-    // }
-    // std::cout << startVertex << std::endl;
-
-    return 0;
+    return pathsNumber[endVertex];
 }
 
-void run(std::istream& input) {
+int main(int argc, char const *argv[]) {
     int graphSize = 0;
-    input >> graphSize;
+    std::cin >> graphSize;
     assert(graphSize <= 50000);
 
     int edgesCount = 0;
-    input >> edgesCount;
+    std::cin >> edgesCount;
     assert(edgesCount <= 200000);
 
     ListGraph graph(graphSize);
     for (int i = 0; i != edgesCount; i++) {
         int from = 0;
         int to = 0;
-        input >> from >> to;
+        std::cin >> from >> to;
         graph.addEdge(from, to);
     }
 
     int startVertex = -1;
     int endVertex = -1;
-    input >> startVertex >> endVertex;
-    int ShortestPathsNumber = countShortestPaths(graph, startVertex, endVertex);
-    // for (int i = 0; i != graph.size(); i++) {
-    //     std::cout << std::setw(3) << i << " vertecies: " << graph.getVertices(i) << std::endl;
-    // }
-}
-
-void test() {
-    run(std::cin);
-/*    {
-        std::stringstream input;
-        input << "19" << std::endl;  //  vertices
-        input << "37" << std::endl;  //  edges
-
-        input << "0 16" << std::endl;
-        input << "1 0" << std::endl;
-        input << "1 15" << std::endl;
-        input << "1 18" << std::endl;
-        input << "1 9" << std::endl;
-        input << "2 18" << std::endl;
-        input << "3 12" << std::endl;
-        input << "3 13" << std::endl;
-        input << "3 2" << std::endl;
-        input << "4 10" << std::endl;
-        input << "4 17" << std::endl;
-        input << "5 17" << std::endl;
-        input << "5 4" << std::endl;
-        input << "6 5" << std::endl;
-        input << "7 0" << std::endl;
-        input << "7 16" << std::endl;
-        input << "7 6" << std::endl;
-        input << "8 5" << std::endl;
-        input << "8 6" << std::endl;
-        input << "9 8" << std::endl;
-        input << "10 13" << std::endl;
-        input << "10 2" << std::endl;
-        input << "11 12" << std::endl;
-        input << "11 15" << std::endl;
-        input << "12 13" << std::endl;
-        input << "12 2" << std::endl;
-        input << "13 4" << std::endl;
-        input << "14 15" << std::endl;
-        input << "14 7" << std::endl;
-        input << "15 0" << std::endl;
-        input << "16 8" << std::endl;
-        input << "16 9" << std::endl;
-        input << "17 10" << std::endl;
-        input << "17 8" << std::endl;
-        input << "17 9" << std::endl;
-        input << "18 12" << std::endl;
-        input << "18 9" << std::endl;
-
-        input << "14 10" << std::endl;
-
-        run(input);
-    }*/
-    /*{
-        std::stringstream input;
-        input << "6" << std::endl;  //  vertices
-        input << "7" << std::endl;  //  edges
-
-        input << "0 1" << std::endl;
-        input << "0 2" << std::endl;
-        input << "0 3" << std::endl;
-        input << "1 4" << std::endl;
-        input << "2 4" << std::endl;
-        input << "3 4" << std::endl;
-        input << "5 4" << std::endl;
-
-        input << "0 5" << std::endl;  // route
-
-        run(input);
-    }*/
-/*    {
-        std::stringstream input;
-        input << "7" << std::endl;  //  vertices
-        input << "8" << std::endl;  //  edges
-
-        input << "0 1" << std::endl;
-        input << "0 2" << std::endl;
-        input << "1 3" << std::endl;
-        input << "2 3" << std::endl;
-        input << "3 4" << std::endl;
-        input << "3 5" << std::endl;
-        input << "4 6" << std::endl;
-        input << "5 6" << std::endl;
-
-        input << "0 6" << std::endl;  // route
-
-        run(input);
-    }*/
-}
-
-int main(int argc, char const *argv[]) {
-    test();
+    std::cin >> startVertex >> endVertex;
+    std::cout << countShortestPaths(graph, startVertex, endVertex) << std::endl;
     return 0;
 }
